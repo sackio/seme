3dfbd40390e3e4b9a8fd92404ec47b78ad4a9c87
Ben Sack <ben@sack.io>
2014-09-30 11:27:01 -0400

    faker updated

--- test/seme_test.js
+++ test/seme_test.js
@@ -7,27 +7,6 @@ var Seme = new require('../lib/seme.js')()
   , FSTK = require('fstk')
 ;
 
-
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
 exports['seme'] = {
   setUp: function(done) {
     // setup here
@@ -84,7 +63,7 @@ exports['seme'] = {
         return cb();
       }
     , function(cb){
-        test.ok(Seme.services.faker.Name.firstName());
+        test.ok(Seme.services.faker.name.firstName());
         return cb();
       }
     , function(cb){
